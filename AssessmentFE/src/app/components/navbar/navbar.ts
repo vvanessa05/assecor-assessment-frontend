070ab3a36c54f1099a0cc9f5580a4e826fd7c069
vvanessa05 <v.vanessa05@gmail.com>
2025-12-24 18:05:32 +0100

    navbar implemented

--- AssessmentFE/src/app/components/navbar/navbar.ts
+++ AssessmentFE/src/app/components/navbar/navbar.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { links } from 'src/app/utils/constants';
+import { NavbarButton } from "./navbar-button/navbar-button";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NavbarButton],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {
 
+/**
+ * Component representing the navbar
+ */
+export class Navbar {
+  /**
+   * Contains the links of the navbar
+   */
+  links = links;
 }
