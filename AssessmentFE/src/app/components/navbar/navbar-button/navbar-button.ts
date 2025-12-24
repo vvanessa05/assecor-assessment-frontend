41010b4a5b80a6ad38e7a81cd0a8ca9873df5e55
vvanessa05 <v.vanessa05@gmail.com>
2025-12-24 17:14:37 +0100

    unnecessary code removed

--- AssessmentFE/src/app/components/navbar/navbar-button/navbar-button.ts
+++ AssessmentFE/src/app/components/navbar/navbar-button/navbar-button.ts
@@ -1,12 +1,13 @@
-import { Component, HostListener, input, signal } from "@angular/core";
+import { Component, computed, HostListener, inject, input, signal } from "@angular/core";
 import { NavbarLink } from "../../../models/navbar-link";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { MatIconModule } from "@angular/material/icon";
+import { TranslateModule, TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: "app-navbar-button",
-  imports: [CommonModule, RouterModule, MatIconModule],
+  imports: [CommonModule, RouterModule, MatIconModule, TranslateModule],
   templateUrl: "./navbar-button.html",
   styleUrl: "./navbar-button.scss",
 })
