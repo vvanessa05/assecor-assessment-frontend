468f28988a47f7e8c5d3475182e4cff3d78eda9c
vvanessa05 <v.vanessa05@gmail.com>
2025-12-24 17:08:19 +0100

    appConfig improved

--- AssessmentFE/src/app/app.config.ts
+++ AssessmentFE/src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app.routes";
+import { TranslateModule } from "@ngx-translate/core";
+import { provideHttpClient } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
+    importProvidersFrom(TranslateModule.forRoot()),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
   ]
 };
