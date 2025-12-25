4919b7c48b2648d47eef6e0ecf0cf2ad39fc95eb
vvanessa05 <v.vanessa05@gmail.com>
2025-12-25 17:22:36 +0100

    home link added

--- AssessmentFE/src/app/utils/constants.ts
+++ AssessmentFE/src/app/utils/constants.ts
@@ -5,18 +5,23 @@ import { NavbarLink } from "../models/navbar-link";
  */
 export const links: NavbarLink[] = [
   {
+    icon: "home",
+    label: "NAVBAR.HOME",
+    route: "/home"
+  },
+  {
     icon: "movie",
     label: "NAVBAR.MOVIES",
-    route: "movies"
+    route: "/movies"
   },
   {
     icon: "person",
     label: "NAVBAR.CHARACTERS",
-    route: "characters"
+    route: "/characters"
   },
   {
     icon: "scatter_plot",
     label: "NAVBAR.PLANETS",
-    route: "planets"
+    route: "/planets"
   },
 ];
