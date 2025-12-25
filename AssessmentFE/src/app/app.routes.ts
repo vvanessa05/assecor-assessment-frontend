fd6a1bb405f330fc57b1f85547b25a0406b4404d
vvanessa05 <v.vanessa05@gmail.com>
2025-12-25 17:23:44 +0100

    first routes added

--- AssessmentFE/src/app/app.routes.ts
+++ AssessmentFE/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { Home } from "./pages/home/home";
+import { Movies } from "./pages/movies/movies";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "home",
+    component: Home
+  },
+  {
+    path: "movies",
+    component: Movies
+  },
+  {
+    path: "**",
+    redirectTo: "home"
+  }
+];
