d9b9ef53347916b0f750d2df6719187a2286b4af
vvanessa05 <v.vanessa05@gmail.com>
2025-12-26 19:27:46 +0100

    routes added

--- AssessmentFE/src/app/app.routes.ts
+++ AssessmentFE/src/app/app.routes.ts
@@ -1,18 +1,37 @@
 import { Routes } from "@angular/router";
 import { Home } from "./pages/home/home";
 import { Movies } from "./pages/movies/movies";
+import { MovieDetail } from "./pages/movies/movie-detail/movie-detail";
+import { Characters } from "./pages/characters/characters";
+import { CharacterDetail } from "./pages/characters/character-detail/character-detail";
 
 export const routes: Routes = [
   {
     path: "home",
-    component: Home
+    component: Home,
   },
   {
     path: "movies",
-    component: Movies
+    component: Movies,
+    children: [
+      {
+        path: "details/:movieId",
+        component: MovieDetail,
+      },
+    ],
+  },
+    {
+    path: "characters",
+    component: Characters,
+    children: [
+      {
+        path: "details/:characterId",
+        component: CharacterDetail,
+      },
+    ],
   },
   {
     path: "**",
-    redirectTo: "home"
-  }
+    redirectTo: "home",
+  },
 ];
