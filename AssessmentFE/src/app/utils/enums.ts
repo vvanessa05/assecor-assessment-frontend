46d9ae2efc8da2092aacfb8d296c7d463496e60d
vvanessa05 <v.vanessa05@gmail.com>
2025-12-26 14:14:25 +0100

    enums created

---
+++ AssessmentFE/src/app/utils/enums.ts
@@ -0,0 +1,19 @@
+/**
+ * Defines the specific type of toast message to be rendered
+ */
+export enum ToastType {
+  Error,
+  Warning,
+  Success,
+}
+
+/**
+ * Defines the type of the value for the input fields
+ */
+export enum InputValueType {
+  TextArea = "textarea",
+  Number = "number",
+  Date = "date",
+  Text = "text",
+  Selection = "selection"
+}
